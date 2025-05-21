e41e457061c4e65d49bd14f859bea1f01f07b1e2
Jason Bennett <jkjbennett@gmail.com>
2025-05-20 20:40:28 -0500

    Call onReorderEnd on cancelReorder()

--- lib/src/builder/reorderable_animated_builder.dart
+++ lib/src/builder/reorderable_animated_builder.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -364,6 +365,7 @@ class ReorderableAnimatedBuilderState extends State<ReorderableAnimatedBuilder>
   }
 
   void cancelReorder() {
+    widget.onReorderEnd?.call(_insertIndex ?? 0);
     setState(() {
       _dragReset();
     });
